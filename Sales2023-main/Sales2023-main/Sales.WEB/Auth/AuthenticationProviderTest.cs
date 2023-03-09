b2213c582d340dfaee12c5a1e893cfb39a56bf86
Percibal Vasquez <vasquez.percibal@gmail.com>
2023-03-09 11:33:58 -0300

    seguridadharcodeenweb

---
+++ Sales2023-main/Sales2023-main/Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public async override  Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            
+            var anonimous = new ClaimsIdentity();
+            var PerciUser = new ClaimsIdentity(new List<Claim>
+            {
+            new Claim("FirstName", "Percibal"),
+            new Claim("LastName", "Vasquez"),
+            new Claim(ClaimTypes.Name, "vasquez.percibal@gmail.com"),
+            new Claim(ClaimTypes.Role, "Admin")
+            },
+            authenticationType: "test");
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+        }
+    }
+
+}
